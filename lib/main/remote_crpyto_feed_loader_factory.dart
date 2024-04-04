32577e513d596c950c8cef300150e08dd22eae2c
myaasiinh <myaasiinh@gmail.com>
2024-04-04 14:01:36 +0700

    recactor to adjust in feedloader, but still error

--- lib/main/remote_crpyto_feed_loader_factory.dart
+++ lib/main/remote_crpyto_feed_loader_factory.dart
@@ -1,8 +1,9 @@
 import 'package:crypto_app/api/load_crypto_feed_remote_use_case.dart';
 import 'package:crypto_app/main/crypto_feed_dio_client_factory%20.dart';
+import 'package:crypto_app/api/remote_crypto_feed.dart';
 
 class RemoteCryptoFeedLoaderFactory {
-  static LoadCryptoFeedRemoteUseCases createRemoteCryptoFeedLoader() {
+  static CryptoFeedLoader createRemoteCryptoFeedLoader() {
     return LoadCryptoFeedRemoteUseCases(
       CryptoFeedDioClientFactory.createCryptoFeedDioClient(),
     );
