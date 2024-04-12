0db0f53300ec7f56f31a15b5a9c970baa7b17ea8
myaasiinh <myaasiinh@gmail.com>
2024-04-12 17:04:15 +0700

    Add coinimageUrl field to CryptoFeedItemViewModel

--- lib/presentation/cryptofeed_item_viewmodel.dart
+++ lib/presentation/cryptofeed_item_viewmodel.dart
@@ -1,12 +1,14 @@
 import 'package:crypto_app/domain/crypto_feed.dart';
 
 class CryptoFeedItemViewModel {
+  final String coinimageUrl;
   final String coinFullName;
   final String coinName;
   final double price;
   final double changePctDay;
 
   CryptoFeedItemViewModel({
+    required this.coinimageUrl,
     required this.coinFullName,
     required this.coinName,
     required this.price,
@@ -15,6 +17,7 @@ class CryptoFeedItemViewModel {
   
   factory CryptoFeedItemViewModel.fromDomain(Datum datum) {
     return CryptoFeedItemViewModel(
+      coinimageUrl: datum.coinInfo.imageUrl,
       coinFullName: datum.coinInfo.fullName,
       coinName: datum.coinInfo.name,
       price: datum.raw.usd.price,
